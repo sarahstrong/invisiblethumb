62894d4f96e46296038ac009086bb91ae30dbb40
Sarah Strong <sarah.e.strong@gmail.com>
2012-06-02 16:21:44 -0700

    Moved test into its own fct

--- sustainability.js
+++ sustainability.js
@@ -1,8 +1,11 @@
-(function( dom, jq ){
+testTitleChange(document, $);
+
+function testTitleChange( dom, jq ){
 	// should only appear on the sites for which we've permissions
 	dom.title = "this is just a test";
 	if( jq.inArray( 1, [1, 2, 3]) >= 0 ){
 		console.debug("successfully loaded jquery!");
 	}
-})( document, $ );
+
+};
 
