1fb9907e1edae9da0e65c925a1731252bfaadabb
Ryan Gantt <ryan@zuttonet.com>
2012-06-02 18:15:30 -0700

    refactored the Nike API handler out into an object prototype, and implemented exact match search

--- material_request.js
+++ material_request.js
@@ -1,17 +1,50 @@
 /**
- * request handler
+ * Define the object constructor
  */
-var apiKey = "rhok";
-var endPoint = "http://nikemsiapi.nikebetterworld.com/materials/list/key:"+apiKey+"/.json";
-function materialRequest( url, materialName, responseCallback ) {
-	$.get( url, function( data ) {
-		responseCallback(data.data);
-	});
+var NikeAPI = function( endPoint, key, contentType ) {
+	this.endPoint = endPoint;
+	this.key = key;
+	this.contentType = contentType;
 };
 
 /**
- * response handler
+ * Define the object API
  */
-var materialResult = materialRequest( endPoint, "foo", function( materials ) {
-	console.debug( materials );
-});
+NikeAPI.prototype = {
+	/**
+	 * Public API
+	 */
+	getMaterial: function( materialName, callback ) {
+		this._grabMaterialList( materialName, this._exactMatch, callback );
+	},
+	
+	searchMaterials: function( materialQuery, callback ) {
+		this._grabMaterialList( materialQuery, this._fuzzyMatch, callback );
+	},
+	
+	/**
+	 * "Private" methods
+	 */
+	_grabMaterialList: function( query, callback, responseCallback ) {
+		$.get( this._assembleUrl(), function( data ) {
+			responseCallback( callback( query, data.data ) );
+		});
+	},
+	
+	_exactMatch: function( name, data ) {
+		for( var i = 0; i < data.length; i++ ) {
+			if( data[i].Material.name === name ) {
+				return data[i];
+			}
+		}
+		return null;
+	},
+	
+	_fuzzyMatch: function( query, data ) {
+		console.debug("trying to do a fuzzy search on " + query );
+	},
+	
+	_assembleUrl: function() {
+		return this.endPoint + "/key:" + this.key + "/" + this.contentType;
+	},
+};
