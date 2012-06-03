3878f73679cdb4369c1fbc97f043f13452b0896a
Ryan Gantt <ryan@zuttonet.com>
2012-06-02 17:16:36 -0700

    request/response handler code

---
+++ material_request.js
@@ -0,0 +1,17 @@
+/**
+ * request handler
+ */
+var apiKey = "rhok";
+var endPoint = "http://nikemsiapi.nikebetterworld.com/materials/list/key:"+apiKey+"/.json";
+function materialRequest( url, materialName, responseCallback ) {
+	$.get( url, function( data ) {
+		responseCallback(data.data);
+	});
+};
+
+/**
+ * response handler
+ */
+var materialResult = materialRequest( endPoint, "foo", function( materials ) {
+	console.debug( materials );
+});
